e3b3100b011b5ddf0d3603bfe81216ae17ad57df
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-04 22:20:36 -0600

    Eventos , focus

--- js/eventos.js
+++ js/eventos.js
@@ -2,5 +2,53 @@
 
 //Eventos del ratón
 
+function cambiarColor(){
+    var bg = boton.style.background;
+    console.log("Haz presionado el boton");
+    if(bg == "green"){
+        boton.style.background ="red";
+    }
+    else{
+        boton.style.background ="green";
+    }
+    boton.style.padding="10px";
+    boton.style.border="1px solid #ccc"
 
-var boton = document.querySelector("#boton"):
+    return true;
+}
+var boton = document.querySelector("#boton");
+
+//Click
+boton.addEventListener('click', () =>{
+    cambiarColor();
+});
+
+// Mouse over 
+boton.addEventListener('mouseover', ()=>{
+    boton.style.background = "yellow";
+});
+
+//Mouse out 
+boton.addEventListener('mouseout', () =>{
+    boton.style.background = '#ccc';
+});
+
+
+// Focus
+var input = document.querySelector("#campoNombre");
+
+input.addEventListener('focus', ()=>{
+    console.log("Estas dentro del input");
+});
+
+// Blur
+
+
+// Keydown
+
+
+// Keypress
+
+
+
+// keyup
