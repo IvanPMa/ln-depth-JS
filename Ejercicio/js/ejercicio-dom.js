ace8eb8b212a0c03276fb1f5118de3ca02d140f3
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-06 15:27:53 -0600

    Ejercicio DOM Fixed

--- Ejercicio/js/ejercicio-dom.js
+++ Ejercicio/js/ejercicio-dom.js
@@ -1,28 +1,35 @@
 'use strict'
 
 window.addEventListener('load', function(){
-    console.log ("DOM cargado");
+
+    console.log("DOM cargado");
 
 
     var formulario = document.querySelector('#formulario');
     var box_dashed = document.querySelector('.dashed');
 
-    box_dashed.style.display = "none";
+    box_dashed.style.display = 'none';
 
-    formulario.addEventListener('submit',function(){
-        console.log("Evento submit capturado");
 
-        var nombre = document.querySelector("#nombre").value;
-        var apellidos = document.querySelector("#apellidos").value;
-        var edad = document.querySelector("#edad").value;
-        box_dashed.style.display = "block";
+    formulario.addEventListener('submit', function(){
+        console.log("Formulario enviado");
+
+        var nombre= document.querySelector('#nombre').value;
+        var apellidos = document.querySelector('#apellidos').value;
+        var edad = document.querySelector('#edad').value;
+        console.log("Nombre: "+nombre+", Apellidos: "+apellidos+", Edad: "+edad);
 
         var p_nombre = document.querySelector('#p_nombre span');
         var p_apellidos = document.querySelector('#p_apellidos span');
-        var p_nombre = document.querySelector('#p_edad span');
+        var p_edad = document.querySelector('#p_edad span');
+
         p_nombre.innerHTML = nombre;
         p_apellidos.innerHTML = apellidos;
-        p_nombre.innerHTML = edad;        
+        p_edad.innerHTML = edad;
+
+        box_dashed.style.display = 'block';
+
+
         /* Forma uno de mostrar los datos en el dashed
         var datos_usuario = [nombre,apellidos,edad];
         for (var indice in datos_usuario){
@@ -31,6 +38,8 @@ window.addEventListener('load', function(){
             box_dashed.append(parrafo);
         }
         */
+    
     });
+
 
 });
