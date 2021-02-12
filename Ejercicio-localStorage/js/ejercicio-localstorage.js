8744e5b43623be265dd656a1eaccea56f3d5dad1
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-11 23:11:52 -0600

    Form delete movie

--- Ejercicio-localStorage/js/ejercicio-localstorage.js
+++ Ejercicio-localStorage/js/ejercicio-localstorage.js
@@ -15,10 +15,24 @@ formulario.addEventListener('submit', function(){
 });
 
 var ul = document.querySelector('#peliculas-list')
-    for (var i in localStorage ){
-        var li = document.createElement("li");
-        if(typeof localStorage[i] == 'string'){
-            li.append(localStorage[i]);
-            ul.append(li);
-        }       
+for (var i in localStorage ){
+    var li = document.createElement("li");
+    if(typeof localStorage[i] == 'string'){
+        li.append(localStorage[i]);
+        ul.append(li);
+    }       
+}
+
+var  formulariob =document.querySelector('#formBorrarPeliculas');
+
+formulariob.addEventListener('submit', function(){
+        var titulo = document.querySelector('#borrarPelicula').value;
+        
+        
+        if (titulo.trim() != 0){
+        localStorage.removeItem(titulo);
+
     }
+
+    
+});
