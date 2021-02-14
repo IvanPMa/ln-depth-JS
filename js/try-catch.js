2688a4e3217afffd6d4ba6490e01a1910ee69df4
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-13 19:58:29 -0600

    capturar error

--- js/try-catch.js
+++ js/try-catch.js
@@ -2,11 +2,10 @@
 try{
     var year = 2019;
     alert (year);
-    var vector = new Array(99999999999999999);
     
 }catch(error){
     console.log(error);
-    alert('A ocurrido un error en el código')
+    // alert('A ocurrido un error en el código')
 }
 
 
