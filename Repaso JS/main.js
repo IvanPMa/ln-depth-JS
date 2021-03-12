22ee7afd88bc5bb62e705359f03584db015bac23
IvanPM <pachecomaldonadoivan@gmail.com>
2021-03-12 01:01:50 -0600

    Template string

--- Repaso JS/main.js
+++ Repaso JS/main.js
@@ -1,1 +1,12 @@
-alert("Hola mundo");
+var nombre = "Ivan Pacheco"; 
+
+var altura = 170;
+
+var concatenacion = nombre + " " + altura;
+ var datos = document.getElementById("datos");
+ datos.innerHTML = `
+    <h1>Soy la caja de datos </h1>
+    <h2> Mi nombre es: ${nombre} </h2>
+
+    <h3>Mido: ${altura} cm </h3> 
+ `;
