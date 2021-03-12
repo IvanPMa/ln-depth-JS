c2059d7754dc692b521341ac9fc84f2f9573d66f
IvanPM <pachecomaldonadoivan@gmail.com>
2021-03-12 01:07:00 -0600

    Estructura de control - Repaso

--- Repaso JS/main.js
+++ Repaso JS/main.js
@@ -10,3 +10,10 @@ var concatenacion = nombre + " " + altura;
 
     <h3>Mido: ${altura} cm </h3> 
  `;
+
+
+ if(altura >= 190 ){
+     datos.innerHTML = "<h1> Eres una persona alta </h1>;"
+ }else {
+     datos.innerHTML += "<h1>Eres una persona bajita</h1>"
+ }
